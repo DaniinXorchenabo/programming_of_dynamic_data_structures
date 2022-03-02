7cc8216997e9db2952d3bbe7c405220b0320eee2
DaniinXorchenabo <rkbcu@mail.ru>
2022-03-02 18:40:34 +0300

    добавлены атрибуты `[[nodiscard]]` в некоторые функции.

--- lab_2/lab_2_6.h
+++ lab_2/lab_2_6.h
@@ -38,7 +38,7 @@ bool is_empty_set(NODE *testing);
 
 bool check_item(int element, NODE *testing);
 
-NODE *add_item(int new_element, NODE *list_, bool check_element_func(int) = [](int i) { return true; });
+[[nodiscard]] NODE *add_item(int new_element, NODE *list_, bool check_element_func(int) = [](int i) { return true; });
 
 NODE *create_set(int item_count, int min_, int max_, bool check_element_func(int));
 
@@ -46,21 +46,21 @@ int len(NODE *set_);
 
 std::string set_as_string(NODE *set_, const std::string &splitter = (", "));
 
-NODE *delete_set(NODE *set_);
+[[nodiscard]] NODE *delete_set(NODE *set_);
 
 bool is_subset(NODE *subset, NODE *base_set);
 
 bool is_equal(NODE *one, NODE *two);
 
-NODE *union_sets(NODE *small_, NODE *big);
+[[nodiscard]] NODE *union_sets(NODE *small_, NODE *big);
 
-NODE *glue_sets(NODE *big, NODE *small_);
+[[nodiscard]] NODE *glue_sets(NODE *big, NODE *small_);
 
-NODE *intersection_of_sets(NODE *big, NODE *small_);
+[[nodiscard]] NODE *intersection_of_sets(NODE *big, NODE *small_);
 
-NODE *subtraction_of_sets(NODE *reduced, NODE *subtracted);
+[[nodiscard]] NODE *subtraction_of_sets(NODE *reduced, NODE *subtracted);
 
-NODE *symmetric_difference_of_sets(NODE *one, NODE *two);
+[[nodiscard]] NODE *symmetric_difference_of_sets(NODE *one, NODE *two);
 
 
 #endif //PROGRAMMING_OF_DYNAMIC_DATA_STRUCTURES_LAB_2_6_H
