35c903b4b6545ab0474b55ac20a51cb669005e0e
DaniinXorchenabo <rkbcu@mail.ru>
2022-04-13 22:11:49 +0300

    added a realization of all methods in 5th laboratory work.

--- lab_5/SetLab5.cpp
+++ lab_5/SetLab5.cpp
@@ -14,12 +14,89 @@ namespace vw = ranges::views;
 
 
 bool SetLab5::check_item(int element) {
-    return  container.find(element)  != container.end();
+    return container.find(element) != container.end();
 }
 
 int SetLab5::len() {
     return container.size();
 }
+
+bool SetLab5::is_empty_set() const {
+    return container.empty();
+}
+
+bool SetLab5::add_item(int new_element) {
+    if (check_element_func(new_element) && !check_item(new_element)) {
+        container.insert(new_element);
+        return true;
+    }
+    return false;
+}
+
+
+string SetLab5::set_as_string(const std::string &splitter) {
+
+    if (is_empty_set()) {
+        return "<empty set>";
+    }
+    string data;
+    for (auto item: container) {
+        if (!data.empty()) {
+            data += splitter;
+        }
+        data += std::to_string(item);
+    }
+    return "(<" + data + ">)";
+
+}
+
+bool SetLab5::is_subset(SetLab5 *subset) {
+    return rng::includes(container, subset->container);
+}
+
+bool SetLab5::is_equal(SetLab5 *other) {
+    return is_subset(other) && other->is_subset(this);
+}
+
+
+SetLab5 *SetLab5::union_sets(SetLab5 *other) {
+    auto result = new SetLab5([this, other](int i) {
+        return this->check_element_func(i) || other->check_element_func(i);
+    });
+    list<int> d = {};
+    rng::set_union(container, other->container, std::inserter(result->container, result->container.begin()));
+    return result;
+}
+
+SetLab5 *SetLab5::intersection_of_sets(SetLab5 *other) {
+    auto result = new SetLab5([this, other](int i) {
+        return this->check_element_func(i) && other->check_element_func(i);
+    });
+    rng::set_intersection(container, other->container, std::inserter(result->container, result->container.begin()));
+    return result;
+}
+
+SetLab5 *SetLab5::subtraction_of_sets(SetLab5 *subtracted) {
+    return subtraction_of_sets(subtracted, static_cast<function<bool(int)>>(check_element_func));
+}
+
+SetLab5 *SetLab5::subtraction_of_sets(SetLab5 *subtracted, const function<bool(int)> &function1) {
+    auto result = new SetLab5(static_cast<function<bool(int)>>(function1));
+    rng::set_difference(container, subtracted->container, std::inserter(result->container, result->container.begin()));
+    return result;
+}
+
+SetLab5 *SetLab5::symmetric_difference_of_sets(SetLab5 *other) {
+    auto result = new SetLab5(
+            [this, other](int i) {
+                return this->check_element_func(i) || other->check_element_func(i);
+            }
+    );
+    rng::set_symmetric_difference(container, other->container,
+                                  std::inserter(result->container, result->container.begin()));
+    return result;
+}
+
 
 SetLab5::SetLab5(function<bool(int)> function1) {
     check_element_func = std::move(function1);
@@ -35,7 +112,7 @@ SetLab5::SetLab5(int item_count, int min_, int max_,
     std::vector<int> second(numbers_out.begin(), numbers_out.end());
 
     rng::shuffle(second, gen);
-    for(auto item: second | vw::take(item_count)){
+    for (auto item: second | vw::take(item_count)) {
         container.insert(item);
     }
 
