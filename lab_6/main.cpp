f9431700c173ef26c3b22697ef3ff1fa69ed8a22
DaniinXorchenabo <rkbcu@mail.ru>
2022-04-15 16:22:24 +0300

    deleted useless comments.

--- lab_6/main.cpp
+++ lab_6/main.cpp
@@ -35,21 +35,6 @@ namespace vw = ranges::views;
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    cout << "Привет" << endl;
-
-//    _setmode(_fileno(stdout), _O_U16TEXT);
-//    _setmode(_fileno(stdin),  _O_U16TEXT);
-//    _setmode(_fileno(stderr), _O_U16TEXT);
-//
-//    std::wcout << L"Unicode -- English -- Русский -- Ελληνικά -- Español." << std::endl;
-    // или
-//    wprintf(L"%s", L"Unicode -- English -- Русский -- Ελληνικά -- Español.\n");
-
-
-//    setlocale(LC_ALL, "Russian");
-//    auto d = SetLab3;
-//    auto classes = {SetLab3,SetLab4, SetLab5 };
-
     vector<vector<string>> printed_array = {
             {"", "Односвязный список", "Класс 'Список'", "List", "Set", "Queue"},
             {"Создание множества"},
@@ -72,14 +57,7 @@ int main() {
             SetAsSet(lennn),
             SetAsQueue(lennn)
     };
-//    list<string> s1 = SetAsNode(lennn);
-//    data_->push_back(s1);
-//    data_->push_back(SetAsNodeClass(lennn));
-//    data_->push_back(SetAsList(lennn));
-//    data_->push_back(SetAsSet(lennn));
-//    data_->push_back(SetAsQueue(lennn));
 
-//    data_[0][0];
     int cell_ind = 0;
     for (auto cell: *data_) {
         int item_ind = 0;
@@ -87,38 +65,23 @@ int main() {
             string item;
             for (auto ch: time_) {
                 item += ch;
-                cout << ch;
             }
             printed_array[item_ind + 1].push_back(item);
-            cout << "\t";
             item_ind++;
         }
-        cout << "\n||||>>>>" << endl;
         cell_ind++;
     }
 
-
-    auto gel_max_len =
-            [printed_array](auto index_, int j){
-                return  static_cast<int>(printed_array[index_][j].size());
-            };
     vector<int> lens = {};
     for(int j = 0; j < printed_array[0].size(); j++){
         int max_len = 0;
         for(int index = 0; index < printed_array.size(); index++){
-            cout << printed_array[index][j].size()<< "|"
-            << (max_len < printed_array[index][j].size()) << "\t";
             if (max_len < printed_array[index][j].size()){
                 max_len = static_cast<int>(printed_array[index][j].size());
             }
         }
-        cout << endl;
         lens.push_back(max_len);
     }
-    for(auto i: lens){
-        cout << i << "\t|";
-    }
-    cout << endl;
 
     for(int index = 0; index < printed_array.size(); index++){
         for(int j = 0; j < printed_array[0].size(); j++){
@@ -127,14 +90,12 @@ int main() {
         printed_array[index][printed_array[0].size() - 1] += "|";
     }
 
-    cout << endl;
 
     int string_len = 0;
     for(auto i: printed_array[0]){
         string_len += static_cast<int>(i.size());
     }
     string middle_border;
-    cout << "**\n";
     for(int i = 0; i < string_len; i++){
         middle_border += "-";
     }
@@ -159,55 +120,5 @@ int main() {
         cout << "#";
     }
     cout << endl;
-
-//    cout<< l_filling("1234", 10, "*") << endl;
-
-//        for(auto ch: time_) {
-//            cout << ch ;
-//        }
-//        cout << "\t";
-//    }
-//    cout << endl;
-//
-//    for (int set_type_index = 0; set_type_index < data_->size(); set_type_index++) {
-//        int index = 0;
-//        for (auto coll: printed_array) {
-//            if (index != 0) {
-//                auto o = data_[set_type_index];
-//                for(const auto& gg: o){
-//                    for (auto ch: gg){
-//                        cout << ch;
-//                    }
-//                    cout << "\t====\t";
-//                }
-//                cout <<endl;
-//                coll.push_back("df");
-//
-//            }
-//            index++;
-//        }
-//    }
-
-    // ==============================================
-
-//
-//    for (const auto &i: SetAsNode(10000)) {
-//        cout << i << endl;
-//    }
-//    for (const auto &i: SetAsNodeClass(10000)) {
-//        cout << i << endl;
-//    }
-//    for (const auto &i: SetAsList(10000)) {
-//        cout << i << endl;
-//    }
-//
-//    for (const auto &i: SetAsSet(10000)) {
-//        cout << i << endl;
-//    }
-//    for (const auto &i: SetAsQueue(10000)) {
-//        cout << i << endl;
-//    }
-    string f;
-//    cin>>f;
     return 0;
 }
